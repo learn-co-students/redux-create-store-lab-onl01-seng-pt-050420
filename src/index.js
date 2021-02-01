25b90ac872e882035bfcb735f97d3f7161c50fa4
danielkwon89 <danielkwon89@gmail.com>
2021-02-01 14:19:54 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -33,6 +33,3 @@ function render() {
     throw new Error("the store's state has not been defined yet")
   }
 };
-
-// Use your createStore function and the functions provided here to create a store.
-// Once the store is created, call an initial dispatch.
